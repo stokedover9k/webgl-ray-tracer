4ea257ed66950c406d7d075ef5c44fdc37120646
Yuriy Skobov <yuriy@yuriy-M17x.(none)>
2013-09-24 01:11:20 -0400

    reflection (1 level deep) works

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function square() { return _shape ( [
         // shiny   metallic   transparency
         [   5.0,      0.4,       0.0,
            15.0,      0.7,       0.7,
-            1.0,      0.3,       0.2 ];
+            7.0,      0.8,       0.2 ];
 
         shaderProgram.sphereLocsUniform = gl.getUniformLocation(shaderProgram, "sphereLocs");
         gl.uniform4fv(shaderProgram.sphereLocsUniform, sphereLocs);
